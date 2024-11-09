746f3b0f287de078f12db0b8a512e518a3fbb105
IriMerkulova <151544565+IriMerkulova@users.noreply.github.com>
2024-11-09 21:32:17 +0300

    Add files via upload
    функция с числами в циклах

---
+++ modul_2_d.py
@@ -0,0 +1,21 @@
+# Слишком древний шифр
+# n = [3:20] = result and n%(i+j)==0
+
+
+def generate_password(n):
+    if n < 3 or n > 20:
+        return "Число должно быть в диапазоне от 3 до 20."
+    password = ""
+    pas_list = []
+    for i in range(1, n):
+        for j in range(i + 1, n + 1):
+            pas_list_sum = i + j
+            if n % pas_list_sum == 0:
+                pas_list.append((i, j))
+    for i, j in pas_list:
+        password += str(i) + str(j)
+    return password
+
+n = int(input("Введите число от 3 до 20: "))
+result = generate_password(n)
+print("Нужный пароль:", result)
